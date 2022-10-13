805fb61db6323511863064cafdd07eb6b49706f9
Uthman <fresher.dev01@gmail.com>
2022-10-13 10:15:49 +0100

    refactor float community

---
+++ components/recentWinners.php
@@ -0,0 +1,201 @@
+<div class="mb-3 card card-sticky" style="height: 100%;">
+    <div class="card-header-tab card-header">
+        <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
+            <i class="header-icon lnr-shirt mr-3 text-muted opacity-6"></i>
+            Recent Float Winners
+        </div>
+    </div>
+
+    <div class="pt-2 pb-0 card-body mt-3">
+        <div class="scroll-area-md shadow-overflow">
+            <div class="scrollbar-container">
+                <ul
+                    class="rm-list-borders rm-list-borders-scroll list-group list-group-flush">
+                    <li class="list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <img width="38" class="rounded-circle"
+                                        src="assets/images/avatars/1.jpg" alt="">
+                                </div>
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Viktor Martin</div>
+                                    <div class="widget-subheading mt-1 opacity-10">
+                                        <div class="badge badge-pill badge-dark">$152
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="fsize-1 text-focus">
+                                        <small class="opacity-5 pr-1">$</small>
+                                        <span>752</span>
+                                        <small class="text-warning pl-2">
+                                            <i class="fa fa-angle-down"></i>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <img width="38" class="rounded-circle"
+                                        src="assets/images/avatars/2.jpg" alt="">
+                                </div>
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Denis Delgado</div>
+                                    <div class="widget-subheading mt-1 opacity-10">
+                                        <div class="badge badge-pill badge-dark">$53
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="fsize-1 text-focus">
+                                        <small class="opacity-5 pr-1">$</small>
+                                        <span>587</span>
+                                        <small class="text-danger pl-2">
+                                            <i class="fa fa-angle-up"></i>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <img width="38" class="rounded-circle"
+                                        src="assets/images/avatars/3.jpg" alt="">
+                                </div>
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Shawn Galloway</div>
+                                    <div class="widget-subheading mt-1 opacity-10">
+                                        <div class="badge badge-pill badge-dark">$239
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="fsize-1 text-focus">
+                                        <small class="opacity-5 pr-1">$</small>
+                                        <span>163</span>
+                                        <small class="text-muted pl-2">
+                                            <i class="fa fa-angle-down"></i>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <img width="38" class="rounded-circle"
+                                        src="assets/images/avatars/4.jpg" alt="">
+                                </div>
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Latisha Allison</div>
+                                    <div class="widget-subheading mt-1 opacity-10">
+                                        <div class="badge badge-pill badge-dark">$21
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="fsize-1 text-focus">
+                                        <small class="opacity-5 pr-1">$</small>
+                                        653
+                                        <small class="text-primary pl-2">
+                                            <i class="fa fa-angle-up"></i>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <img width="38" class="rounded-circle"
+                                        src="assets/images/avatars/5.jpg" alt="">
+                                </div>
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Lilly-Mae White</div>
+                                    <div class="widget-subheading mt-1 opacity-10">
+                                        <div class="badge badge-pill badge-dark">$381
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="fsize-1 text-focus">
+                                        <small class="opacity-5 pr-1">$</small> 629
+                                        <small class="text-muted pl-2">
+                                            <i class="fa fa-angle-up"></i>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <img width="38" class="rounded-circle"
+                                        src="assets/images/avatars/8.jpg" alt="">
+                                </div>
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Julie Prosser</div>
+                                    <div class="widget-subheading mt-1 opacity-10">
+                                        <div class="badge badge-pill badge-dark">$74
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="fsize-1 text-focus">
+                                        <small class="opacity-5 pr-1">$</small>462
+                                        <small class="text-muted pl-2">
+                                            <i class="fa fa-angle-down"></i>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="border-bottom-0 list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <img width="38" class="rounded-circle"
+                                        src="assets/images/avatars/8.jpg" alt="">
+                                </div>
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Amin Hamer</div>
+                                    <div class="widget-subheading mt-1 opacity-10">
+                                        <div class="badge badge-pill badge-dark">$7
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="fsize-1 text-focus">
+                                        <small class="opacity-5 pr-1">$</small>
+                                        956
+                                        <small class="text-success pl-2">
+                                            <i class="fa fa-angle-up"></i>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="d-block text-center rm-border card-footer">
+        
+    </div>
+</div>
